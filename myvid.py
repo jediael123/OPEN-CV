2ee96bc7e36733ebdf399e73b750d2e706773840
jediael123 <priscilladear1@yahoo.com>
2026-08-01 13:49:47 +0100

    Add files via upload

--- myvid.py
+++ myvid.py
@@ -11,7 +11,8 @@ count = 0
 for i in os.listdir("."):
     if i.endswith(".png") or i.endswith(".jpg"):
         count+=1
-        img = Image.open(os.path.join(path,i))
+        print(i)
+        img = Image.open(i)
         w,h = img.size
         meanheight+=h
         meanwidth+=w
@@ -20,6 +21,20 @@ meanheight = meanheight//count
 
 for i in os.listdir("."):
     if i.endswith(".png") or i.endswith(".jpg"):
-        img = Image.open(os.path.join(path,i))
+        img = Image.open(i)
         img = img.resize((meanwidth,meanheight),Image.Resampling.LANCZOS)
-        img.save(i,"JPEG",quality=95)
+        img.save(i,"PNG",quality=95)
+
+def videoGenerator():
+    videoname = "myvideo.avi"
+    images = []
+    for i in os.listdir("."):
+        if i.endswith(".png") or i.endswith(".jpg"):
+            images.append(i)
+    video = cv2.VideoWriter(videoname,0,1,(meanwidth,meanheight))
+    for i in images:
+        video.write(cv2.imread(os.path.join(".",i)))
+    cv2.destroyAllWindows()
+    video.release()
+videoGenerator()
+        
